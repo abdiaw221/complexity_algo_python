4a62f12f4b78ad3ca025389ba5c230a4d903e7ea
abbcr <abdiaw221@gmail.com>
2024-03-21 01:07:22 +0000

    TP_1 chaine de cataract

--- README.md
+++ README.md
@@ -1,1 +1,28 @@
-# complexity_algo_python
+# Python Sorting Algorithm Complexity
+
+This project contains everything required to develop and execute sorting algorithms.
+
+```
+/requirements.txt    - Useful dependencies for local execution
+/venv                - Python virtual environment
+```
+
+# Setup
+
+To build and execute the project, ensure you have the following environment:
+
+- python 3.11.5 or higher
+
+# Running Locally
+
+To run the project locally, follow these steps:
+
+1) Install necessary dependencies by executing:
+
+```
+pip install -r requirement.txt
+```
+
+2) Launch the project using any of the following methods:
+
+- Run the main.py class in your editor under the algorithm_tri package.
---
+++ td_tp_1/exercice_1.py
@@ -0,0 +1,33 @@
+def get_occurrence_number(chaine, character):
+    number = 0;
+    for letter in chaine:
+        if letter == character:
+            number += 1
+    return number
+
+
+def get_occurrence_number_count(chaine, character):
+    return chaine.count(character)
+
+
+def first_position_of_occurrence(chaine, character):
+    for i, letter in enumerate(chaine):
+        if letter == character:
+            return i
+    return -1
+
+
+def get_message_with_interval_code_ascii(number_1, number_2):
+    try:
+        number_1 = int(number_1)
+        number_2 = int(number_2)
+    except ValueError:
+        return "Input values must be integers."
+
+    message = ""
+    if number_1 < number_2:
+        for i in range(number_1, number_2 + 1):
+            message += "{} - ".format(chr(i))
+    else:
+        message = "{} is less than {}".format(number_2, number_1)
+    return message
