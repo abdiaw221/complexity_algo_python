316bd1032d3378a2cc8a08932e92d02f6f5952d5
abbcr <abdiaw221@gmail.com>
2024-04-01 23:27:16 +0000

    TP_3 Crypto App : exercice 2 : return chaines en majuscule en supprimant les charactére spéciaux...

--- td_tp_1/exercice_2.py
+++ td_tp_1/exercice_2.py
@@ -1,4 +1,8 @@
 # Cryptographic : Code de Vigenère
+from unidecode import unidecode
+import re
+
+
 def en_majuscule(chaine):
     chaine_majuscule = ""
     for character in chaine:
@@ -13,6 +17,19 @@ def en_majuscule(chaine):
     return chaine_majuscule
 
 
+def del_special_character(chaine):
+    chaine_propre = re.sub(r'[^a-zA-Z]', '', chaine)
+    return chaine_propre
+
+
+def en_majuscule_only(chaine):
+    majuscule = en_majuscule(chaine)
+    only_majuscule_only = unidecode(majuscule)
+    del_special_characters = del_special_character(only_majuscule_only)
+    return del_special_characters
+
+
 if __name__ == '__main__':
     texts = "Hey Hey, cet été là, le gang des niçois à été arrêté!"
     print(en_majuscule(texts))
+    print(en_majuscule_only(texts))
